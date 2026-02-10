394bcfd8df411262e8b9753d3adcba22a05a86c1
ThiagoMelo22 <thiagomelo329@hotmail.com>
2026-02-10 13:54:57 -0300

    Ajusted

--- Product/Product/obj/Debug/net8.0/Product.AssemblyInfo.cs
+++ Product/Product/obj/Debug/net8.0/Product.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dfd79804bc3b372625c2cb01394ea349f24c6ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
